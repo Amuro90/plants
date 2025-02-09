19048ffc5f35ed628cb40daefead6102edc68d92
Amuro90 <andrea.di.carlo90@gmail.com>
2025-02-09 18:45:43 +0100

    initial commit

---
+++ src/main/java/com/andrea/piante/PianteApplication.java
@@ -0,0 +1,13 @@
+package com.andrea.piante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PianteApplication.class, args);
+	}
+
+}
